3398461a7ee3edf558a738827ef9b2a586499851
Abhishek Naidu <geekyabhi12@gmail.com>
2024-09-02 21:02:42 +0530

    chore: add dashboard page:

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,20 @@
 
 import FundraiserCard from "@/components/FundraiserCard";
 import Header from "@/components/Header";
+import cn from "classnames";
 import { useUser } from "@/hooks/user";
-import { Button, Grid, SegmentedControl, Skeleton, Title } from "@mantine/core";
+import {
+  Badge,
+  Button,
+  Grid,
+  SegmentedControl,
+  Skeleton,
+  Title,
+} from "@mantine/core";
 import { IconPlus } from "@tabler/icons-react";
 import Link from "next/link";
 import { useMemo, useState } from "react";
+import { prettyFont } from "../fonts";
 
 const TAB_OPTIONS = [
   {
@@ -44,7 +53,22 @@ export default function Page() {
       <Header />
       <div className="flex flex-col w-full gap-4">
         <div className="flex gap-4 justify-between">
-          <Title order={2}>Your Fundraisers</Title>
+          <div className="">
+            <Title
+              // className="text-[#020227]  mb-2 pretty uppercase"
+              className={cn(
+                "text-[#020227] text-[32px] leading-[39px] mt-4 mb-2 uppercase",
+                prettyFont.className
+              )}
+              order={2}
+            >
+              Dashboard
+            </Title>{" "}
+            <div className="text-sm uppercase text-[#010126]">
+              EXPLORE ALL YOUR FUNDRAISERS, GITHUB PROFILES, DRIP, ETC
+            </div>
+          </div>
+
           <Button
             variant="light"
             rightSection={<IconPlus size={20} />}
@@ -54,29 +78,43 @@ export default function Page() {
             Create
           </Button>
         </div>
-        <div>
-          <SegmentedControl
-            value={activeTab}
-            onChange={setActiveTab}
-            data={TAB_OPTIONS}
-            color="primary"
-          />
-        </div>
 
         <Grid gutter={16} pb={100}>
           {isUserLoading || userError || !user
             ? new Array(5).fill(0).map((_, index) => (
-                <Grid.Col key={index} span={4}>
+                <Grid.Col key={index} span={12}>
                   <Skeleton height={200} />
                 </Grid.Col>
               ))
             : funds.map((fundraiser) => (
-                <Grid.Col key={fundraiser.id} span={4}>
-                  <FundraiserCard
-                    data={fundraiser}
-                    userAddress={user.address}
-                  />
-                </Grid.Col>
+                <div className="flex gap-3">
+                  <Grid.Col key={fundraiser.id} span={4}>
+                    <FundraiserCard
+                      data={fundraiser}
+                      userAddress={user.address}
+                    />
+                  </Grid.Col>
+                  <div className="flex flex-col mt-6">
+                    <div className="rounded-[23px] w-[14%] table bg-[#C5DEFF] py-[6px] px-[14px]">
+                      <div className="text-[#16509E] text-[12px] leading-[14px] uppercase">
+                        {fundraiser.type}
+                      </div>
+                    </div>
+                    <Title
+                      className={cn(
+                        "text-[#020227] text-[26px] leading-[39px] mt-4 mb-2 uppercase",
+                        prettyFont.className
+                      )}
+                      order={2}
+                    >
+                      {fundraiser.title}
+                    </Title>
+                    <div className="text-[16px] leading-[18px] text-[#010126]">
+                      {fundraiser.description}
+                    </div>
+                    <div className="mt-10">{`Crowdsrourced: ${fundraiser.raised}`}</div>
+                  </div>
+                </div>
               ))}
         </Grid>
       </div>
