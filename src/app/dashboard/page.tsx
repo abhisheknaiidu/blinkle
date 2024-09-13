f4ef6fbe7ef59772af41a3d44cb4a3493c41e7b6
Raj Varsani <varasaniraj5@gmail.com>
2024-09-13 14:06:23 +0530

    refactor: Update label for custom donation amount field in blink actions and improve styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import { useUser } from "@/hooks/user";
 import { BLINK_TYPE } from "@/types";
 import {
+  ActionIcon,
   Badge,
   Button,
   Flex,
@@ -20,12 +21,15 @@ import {
   IconDroplet,
   IconHeartHandshake,
   IconPlus,
+  IconShadow,
+  IconShare,
 } from "@tabler/icons-react";
 import cn from "classnames";
 import { useMemo, useState } from "react";
 import { prettyFont } from "../fonts";
 import Link from "next/link";
 import useScreen from "@/utils/useScreen";
+import { SERVER_BASE_URL } from "@/utils/constants";
 
 const TAB_OPTIONS = [
   {
@@ -94,6 +98,7 @@ export default function Page() {
       </div>
     );
   }
+
   return (
     <div className="min-h-[100vh] mx-auto max-w-4xl px-10 items-center flex flex-col gap-5">
       <Header />
@@ -168,61 +173,97 @@ export default function Page() {
             ? new Array(5)
                 .fill(0)
                 .map((_, index) => <Skeleton height={200} key={index} />)
-            : funds.map((fundraiser) => (
-                <div
-                  className="gap-8"
-                  key={fundraiser.id}
-                  style={{
-                    display: "grid",
-                    gridTemplateColumns: "auto 1fr",
-                  }}
-                >
-                  <FundraiserCard
-                    data={fundraiser}
-                    userAddress={user.address}
-                  />
+            : funds.map((fundraiser) => {
+                const solanaUrl = `solana-action:${SERVER_BASE_URL}api/actions/blinks?user=${user.address}&blink=${fundraiser.id}`;
+                // https://dial.to/?action=solana-action%3Ahttps%3A%2F%2Frelative-dog-uniquely.ngrok-free.app%2Fapi%2Factions%2Fblinks%3Fuser%3D5sy3ayWTWE7bnXBhrRThTo722q67mzo7T8m43Kk6Y2wr%26blink%3Dc9a81bb3-4e39-4cf5-a26e-d529d7b1571a
+                const dialURL = `https://dial.to/?action=${encodeURIComponent(
+                  solanaUrl
+                )}`;
+                const twitterShareContent = (() => {
+                  switch (fundraiser.type) {
+                    case BLINK_TYPE.FUNDRAISER:
+                      return `Check out my fundraiser on Solana: ${fundraiser.title} ${dialURL}`;
+                    case BLINK_TYPE.DRIP:
+                      return `Check out my drip on Solana: ${fundraiser.title} ${dialURL}`;
+                    case BLINK_TYPE.GITHUB:
+                      return `Check out my github on Solana: ${fundraiser.title} ${dialURL}`;
+                  }
+                })();
+                const twitterShareURL = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                  twitterShareContent
+                )}`;
 
-                  <div className="flex flex-col mt-4">
-                    <Badge
-                      variant="light"
-                      color={
-                        fundraiser.type === BLINK_TYPE.FUNDRAISER
-                          ? "grape"
-                          : fundraiser.type === BLINK_TYPE.DRIP
-                          ? "pink"
-                          : fundraiser.type === BLINK_TYPE.GITHUB
-                          ? "blue"
-                          : "dark"
-                      }
-                      mt={20}
-                      size="lg"
-                    >
-                      {fundraiser.type}
-                    </Badge>
-                    <Title
-                      className={cn(
-                        "text-[#020227] text-[26px] leading-[39px] mt-3 mb-1 uppercase",
-                        prettyFont.className
-                      )}
-                      order={2}
-                      c="#020227"
-                    >
-                      {fundraiser.title}
-                    </Title>
-                    <Text size="sm" c="#020227" opacity={0.4}>
-                      {fundraiser.type === BLINK_TYPE.DRIP
-                        ? renderDripDescription(fundraiser.description)
-                        : fundraiser.description}
-                    </Text>
-                    <Text mt={16} opacity={0.6} c="#020227">
-                      {"Raised: "}
-                      <Text span fw={600}>
-                        {fundraiser.raised} SOL
+                return (
+                  <div
+                    className="gap-8"
+                    key={fundraiser.id}
+                    style={{
+                      display: "grid",
+                      gridTemplateColumns: "auto 1fr",
+                    }}
+                  >
+                    <FundraiserCard
+                      data={fundraiser}
+                      userAddress={user.address}
+                    />
+
+                    <div className="flex flex-col mt-4">
+                      <Badge
+                        variant="light"
+                        color={
+                          fundraiser.type === BLINK_TYPE.FUNDRAISER
+                            ? "grape"
+                            : fundraiser.type === BLINK_TYPE.DRIP
+                            ? "pink"
+                            : fundraiser.type === BLINK_TYPE.GITHUB
+                            ? "blue"
+                            : "dark"
+                        }
+                        mt={20}
+                        size="lg"
+                      >
+                        {fundraiser.type}
+                      </Badge>
+                      <Flex align="center" gap={16}>
+                        <Title
+                          className={cn(
+                            "text-[#020227] text-[26px] leading-[39px] mt-3 mb-1 uppercase",
+                            prettyFont.className
+                          )}
+                          order={2}
+                          c="#020227"
+                        >
+                          {fundraiser.title}
+                        </Title>
+                        <ActionIcon
+                          color="blue"
+                          variant="subtle"
+                          component={Link}
+                          href={twitterShareURL}
+                          target="_blank"
+                        >
+                          <IconShare style={{ height: "60%", width: "60%" }} />
+                        </ActionIcon>
+                      </Flex>
+                      <Text size="sm" c="#020227" opacity={0.4}>
+                        {fundraiser.type === BLINK_TYPE.DRIP
+                          ? renderDripDescription(fundraiser.description)
+                          : fundraiser.description}
                       </Text>
-                    </Text>
+                      <Text mt={16} opacity={0.6} c="#020227" fz={14}>
+                        {"Raised: "}
+                        <Text span fw={600}>
+                          {fundraiser.raised} SOL
+                        </Text>
+                        {" | Views: "}
+                        <Text span fw={600}>
+                          {fundraiser.views} SOL
+                        </Text>
+                      </Text>
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
         </Flex>
       </div>
     </div>
